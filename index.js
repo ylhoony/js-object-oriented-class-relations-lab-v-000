1a09a6267374d871bb146c866d1fbdf438c191ce
Hoon Lee <yl.hoony@gmail.com>
2017-10-10 09:46:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,15 +27,16 @@ let tripId = 0;
 class Trip {
   constructor(driver, passenger) {
     this.id = ++tripId;
-
     if(driver) {
       this.driverId = driver.id;
     }
-
     if(passenger) {
       this.passengerId = passenger.id;
     }
 
     store.trips.push(this);
+  }
+  driver(){
+    return 
   }
 }
