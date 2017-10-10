7f1ab93995ce3a32bf453965d43fdac247dae13a
Hoon Lee <yl.hoony@gmail.com>
2017-10-10 10:00:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 let store = {drivers: [], passengers: [], trips: []};
 
 let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
 
 class Driver {
   constructor(name) {
     this.id = ++driverId;
     this.name = name;
-
     store.drivers.push(this);
   }
 }
-
-let passengerId = 0;
 
 class Passenger {
   constructor(name) {
     this.id = ++passengerId;
     this.name = name;
-
     store.passengers.push(this);
   }
 }
-
-let tripId = 0;
 
 class Trip {
   constructor(driver, passenger) {
